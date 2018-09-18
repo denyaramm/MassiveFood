fad546df46653216fb212f8c03d49567c0fd97f5
unknown <lesnov_d@Lesnov_d.vlkm2.local>
2018-09-18 16:50:44 +0600

    Change one word in MassiveFood

--- src/MassiveFood.java
+++ src/MassiveFood.java
@@ -5,7 +5,7 @@ public class MassiveFood {
             System.out.println(s);
         for (String s : food) {
             if (s.startsWith("м"))
-                System.out.print(s + " начинается с к, ");
+                System.out.print(s + " начинается с а, ");
         }
     }
 }
